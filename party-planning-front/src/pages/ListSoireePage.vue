af7b838a9db7b83942722b2f43d49a6a555b0155
SlayzerZ <ibrahimallari84@gmail.com>
2023-04-14 14:52:42 +0200

    Meme

--- party-planning-front/src/pages/ListSoireePage.vue
+++ party-planning-front/src/pages/ListSoireePage.vue
@@ -27,6 +27,7 @@
         <a>Créer une soirée</a>
       </button>
     </div>
+    <!-- <button type = "button" id = "get-joke" @click = "fetchAPIData">Get a Joke!!</button> -->
     <create-soiree-page v-if="currentPage === 'CreateSoireePage'"/>
     <event-page v-if="currentPage === 'EventPage'" />
     <component :is="currentView" />
@@ -36,12 +37,41 @@
 <script>
 import CreateSoireePage from "@/pages/CreateSoireePage";
 import EventPage from "@/pages/EventPage";
+import Vue from 'vue'
 
 const routes = {
   '/createSoiree': CreateSoireePage,
   '/event/': EventPage,
 }
 export default {
+  app : new Vue({
+    methods: {
+    fetchAPIData( ) { 
+      this.responseAvailable = false;
+fetch("https://jokes-database.p.rapidapi.com/", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "jokes-database.p.rapidapi.com",
+        "x-rapidapi-key": this.apiKey
+    }
+})
+.then(response => { 
+    if(response.ok){
+        return response.json()    
+    } else{
+        alert("Server returned " + response.status + " : " + response.statusText);
+    }                
+})
+.then(response => {
+    this.result = response.body; 
+    this.responseAvailable = true;
+})
+.catch(err => {
+    console.log(err);
+});
+        }
+  }
+  }),
   name: "ListSoireePage",
   components: { CreateSoireePage, EventPage  },
   data() {
@@ -65,13 +95,7 @@ export default {
       this.currentPath = window.location.hash
     })
   },
-  methods: {
-    goToSoiree (soireeId) {
-      this.currentPage = 'EventPage'
-      console.log(soireeId)
-      //alert('id de la soiree :' + soireeId)
-    },
-  }
+  
 
 }
 </script>
