0efce7a831317d3fe21eb8459b5e33c680b9bb9f
SlayzerZ <ibrahimallari84@gmail.com>
2023-04-14 10:03:19 +0200

    Login

--- party-planning-front/src/main.js
+++ party-planning-front/src/main.js
@@ -1,20 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import {VueRouter} from 'vue-router'
-// import Brazil from '@/pages/Brazil.vue'
 
-// Vue.use(VueRouter)
-
-// const routes = [
-//     {path: '/brazil', name: 'brazil', component : Brazil}
-// ]
-
-// const router = new VueRouter({
-//     // history: createWebHistory(),
-//     routes
-// })
-
-// export default router
 Vue.config.productionTip = false
 
 new Vue({
